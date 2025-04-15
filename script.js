9faf0f335384708d93edda4772948c2be69f98cc
Dmytro Lobanov <dspwww@gmail.com>
2025-04-15 11:47:21 +0100

    feat: Add Book contructor & add a book in a list

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log('Hello');
+
+const myLibrary = [];
+
+function Book(title, author, pages) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  
+  this.getInfo = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages`;
+  }
+}
+
+function addBookToLibrary(title, author, pages) {
+  let book = new Book(title, author, pages);
+  myLibrary.push(book);
+}
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295);
+console.log(myLibrary);
