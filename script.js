f1e9dfe8e006b455b78c43b908e71202ac9e7e2d
Dmytro Lobanov <dspwww@gmail.com>
2025-04-15 12:11:42 +0100

    feat: Add list of books and style them

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 console.log('Hello');
 
+const listNode = document.querySelector('.list');
 const myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, year) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.publishYear = 
   
   this.getInfo = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages`;
@@ -18,5 +20,27 @@ function addBookToLibrary(title, author, pages) {
   myLibrary.push(book);
 }
 
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295);
-console.log(myLibrary);
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 310, 1937);
+addBookToLibrary('Harry Potter and the Philosopher\'s Stone', 'J.K. Rowling', 309, 1997);
+
+function createBookNode(book) {
+  const bookNode = document.createElement('div');
+  const bookTitleNode = document.createElement('h4');
+  const bookInfoNode = document.createElement('p');
+
+  bookNode.dataset.id = book.id;
+  bookNode.classList.add('book');
+  bookTitleNode.textContent = book.title;
+  bookInfoNode.textContent = book.getInfo();
+
+  bookNode.appendChild(bookTitleNode);
+  bookNode.appendChild(bookInfoNode);
+
+  return bookNode;
+}
+
+myLibrary.forEach(book => {
+  const bookNode = createBookNode(book);
+
+  listNode.appendChild(bookNode);
+});
