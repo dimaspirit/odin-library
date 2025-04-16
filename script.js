0da92bd753af0d330a3c23a54e6ee22a147ceeac
Dmytro Lobanov <dspwww@gmail.com>
2025-04-16 12:24:19 +0100

    feat: Add read status and toggle function for it

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ console.log('Hello');
 const listNode = document.querySelector('.list');
 let myLibrary = [];
 
-function Book(title, author, pages, year) {
+function Book(title, author, pages, read) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.publishYear = year;
+  this.read = read;
   
   this.getInfo = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages`;
   }
+
+  this.toogleRead = function() {
+    this.read = !this.read;
+  }
 }
 
-function addBookToLibrary(title, author, pages) {
-  let book = new Book(title, author, pages);
+function addBookToLibrary(title, author, pages, read) {
+  let book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
 
@@ -28,27 +32,41 @@ function removeBook(event) {
   renderLibrary(listNode, myLibrary);
 }
 
+function handleToogleRead(book) {
+  book.toogleRead();
+  renderLibrary(listNode, myLibrary);
+}
+
 function createBookNode(book) {
   const bookNode = document.createElement('div');
   const bookTitleNode = document.createElement('h4');
   const bookInfoNode = document.createElement('p');
+  const actionsNode = document.createElement('div');
   const bookRemoveBtnNode = document.createElement('button');
+  const bookReadStatusNode = document.createElement('button');
 
   bookNode.dataset.id = book.id;
   bookNode.classList.add('book');
   bookTitleNode.textContent = book.title;
   bookInfoNode.textContent = book.getInfo();
+  actionsNode.classList.add('book-actions');
   bookRemoveBtnNode.textContent = 'Remove';
   bookRemoveBtnNode.addEventListener('click', removeBook);
+  bookReadStatusNode.textContent = book.read ? 'Mark as not read' : 'Mark as read';
+  bookReadStatusNode.addEventListener('click', () => handleToogleRead(book));
+
+  actionsNode.appendChild(bookRemoveBtnNode);
+  actionsNode.appendChild(bookReadStatusNode);
 
   bookNode.appendChild(bookTitleNode);
   bookNode.appendChild(bookInfoNode);
-  bookNode.appendChild(bookRemoveBtnNode);
+  bookNode.appendChild(actionsNode);
 
   return bookNode;
 }
 
 function renderLibrary(container, books) {
+  // Clean up before render
   while (container.firstChild) {
     container.firstChild.remove();
   }
@@ -61,7 +79,7 @@ function renderLibrary(container, books) {
 }
 
 // Fill library with 2 default books
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 310, 1937);
-addBookToLibrary('Harry Potter and the Philosopher\'s Stone', 'J.K. Rowling', 309, 1997);
-
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 310, false);
+addBookToLibrary('Harry Potter and the Philosopher\'s Stone', 'J.K. Rowling', 309, true);
+console.log('myLibrary', myLibrary)
 renderLibrary(listNode, myLibrary);
